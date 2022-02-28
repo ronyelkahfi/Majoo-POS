96b7557754b16950b85339e82cfed28bd604981e
Fatkhur Rony <ronyelkahfi@gmail.com>
2022-02-28 15:21:56 +0700

    core : add font awesome link

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
+    
     <title>Majoo POS - <?= $title ?></title>
     <?php 
     if(!empty($externalCSS)){
@@ -61,6 +62,7 @@
     
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/48a99d070e.js" crossorigin="anonymous"></script>
     <?php 
     if(!empty($externalJS)){
       foreach ($externalJS as $hrefJS) {
